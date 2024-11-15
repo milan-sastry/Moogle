af10fa4cf2acec9a40f359768d0fc36c12e23a9a
Milan Sastry <milansastry@dynamic-oit-ip4-wifirestricted03-10-49-159-5.princeton.edu>
2024-11-14 20:50:57 -0500

    crawl.ml

--- lib/crawl.ml
+++ lib/crawl.ml
@@ -58,8 +58,28 @@ let print s =
  * Keep crawling until we've
  * reached the maximum number of links (n) or the frontier is empty. *)
 let rec crawl (n:int) (frontier: LinkSet.set)
-    (visited : LinkSet.set) (d:WordDict.dict) : WordDict.dict = 
-  WordDict.empty
+    (visited : LinkSet.set) (d:WordDict.dict) : WordDict.dict =
+  if n = 0 || LinkSet.is_empty frontier then d
+  else
+    match LinkSet.choose frontier with
+    | None -> d
+    | Some (link, frontier') ->
+    if LinkSet.member visited link then crawl n frontier' visited d
+    else
+      match get_page link with
+      | None -> crawl n frontier' (LinkSet.insert link visited) d
+      | Some page ->
+        let words = page.words in
+      let d' = List.fold_left (fun d' word ->
+        let links = 
+          match WordDict.lookup d' word with
+          | Some ls -> ls
+          | None -> LinkSet.empty in
+        let links' = LinkSet.insert link links in
+        WordDict.insert d' word links') d words in
+      let frontier'' = List.fold_left (fun frontier'' link' ->
+        LinkSet.insert link' frontier'') frontier' page.links in
+      crawl (n-1) frontier'' (LinkSet.insert link visited) d'
 ;;
 
 let crawler () = 
